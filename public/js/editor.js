1067ca412b16096c1b10e96b658386d98942eff8
Tom Boutell <tom@punkave.com>
2013-09-27 09:59:16 -0400

    Compatible with the new browser side javascript class pattern

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,40 +1,43 @@
-apos.widgetTypes.twitter = {
-  label: 'Twitter',
-  editor: function(options) {
-    var self = this;
-        
-    if (!options.messages) {
-      options.messages = {};
+// @class Editor for Twitter feed widgets
+
+function AposTwitterWidgetEditor(options) {
+  var self = this;
+
+  if (!options.messages) {
+    options.messages = {};
+  }
+  if (!options.messages.missing) {
+    options.messages.missing = 'Type in a Twitter account name first.';
+  }
+
+  self.type = 'twitter';
+  options.template = '.apos-twitter-editor';
+
+  // Parent class constructor shared by all widget editors
+  AposWidgetEditor.call(self, options);
+
+  // Now we can override methods
+  self.afterCreatingEl = function() {
+    self.$account = self.$el.find('.apos-twitter-account');
+    self.$account.val(self.data.account);
+    setTimeout(function() {
+      self.$account.focus();
+      self.$account.setSelection(0, 0);
+    }, 500);
+  };
+
+  self.prePreview = getAccount;
+  self.preSave = getAccount;
+
+  function getAccount(callback) {
+    self.exists = !!self.$account.val();
+    if (self.exists) {
+      self.data.account = self.$account.val();
     }
-    if (!options.messages.missing) {
-      options.messages.missing = 'Type in a Twitter account name first.';
-    }
+    return callback();
+  }
+}
 
-    self.afterCreatingEl = function() {
-      self.$account = self.$el.find('.apos-twitter-account');
-      self.$account.val(self.data.account);
-      setTimeout(function() {
-        self.$account.focus();
-        self.$account.setSelection(0, 0);
-      }, 500);
-    };
+AposTwitterWidgetEditor.label = 'Twitter';
 
-    self.prePreview = getAccount;
-    self.preSave = getAccount;
-
-    function getAccount(callback) {
-      self.exists = !!self.$account.val();
-      if (self.exists) {
-        self.data.account = self.$account.val();
-      }
-      return callback();
-    }
-
-    self.type = 'twitter';
-    options.template = '.apos-twitter-editor';
-
-    // Parent class constructor shared by all widget editors
-    apos.widgetEditor.call(self, options);
-  },
-};
-
+apos.addWidgetType('twitter');
